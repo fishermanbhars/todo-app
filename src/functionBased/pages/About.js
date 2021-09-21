db9e0ed64604e3776938ccae80b00e2369f33b80
fishermanbhars <bsritharan29@gmail.com>
2021-09-21 15:18:11 -0400

    first commit

---
+++ src/functionBased/pages/About.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link, useRouteMatch, Route } from "react-router-dom"
+import SinglePage from "./SinglePage"
+
+const About = () => {
+  console.log(useRouteMatch())
+  const { url, path } = useRouteMatch()
+  return (
+    <div className="about__content">
+      <ul className="about__list">
+        <li>
+          <Link to={`${url}/about-app`}>About App</Link>
+        </li>
+      </ul>
+      <Route path={`${path}/:slug`}>
+        <SinglePage />
+      </Route>
+    </div>
+  )
+}
+export default About
